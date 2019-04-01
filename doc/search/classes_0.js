11c342806f554a401e2d9a01e42c7b150588b2fe
Cody Rodgers <codrod63@gmail.com>
2019-04-01 00:50:48 -0400

    added docs for pages

---
+++ doc/search/classes_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['cxexception',['cxexception',['../structcxexception.html',1,'']]],
+  ['cxmem_5fstorage',['cxmem_storage',['../structcxmem__storage.html',1,'']]],
+  ['cxstring',['cxstring',['../structcxstring.html',1,'']]],
+  ['cxui',['cxui',['../structcxui.html',1,'']]]
+];
---
+++ doc/search/files_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['cxdef_2eh',['cxdef.h',['../cxdef_8h.html',1,'']]],
+  ['cxerror_2eh',['cxerror.h',['../cxerror_8h.html',1,'']]],
+  ['cxex_2eh',['cxex.h',['../cxex_8h.html',1,'']]],
+  ['cxlib_2eh',['cxlib.h',['../cxlib_8h.html',1,'']]],
+  ['cxmem_2eh',['cxmem.h',['../cxmem_8h.html',1,'']]],
+  ['cxstring_2eh',['cxstring.h',['../cxstring_8h.html',1,'']]]
+];
